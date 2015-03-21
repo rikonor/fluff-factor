fd643bb6b292144af2b21d3dee61e6c9960d2f63
Or Rikon <orikon@augury.com>
2015-03-21 02:02:00 +0200

    Added word menu

--- fluff.js
+++ fluff.js
@@ -20,7 +20,42 @@ if (Meteor.isClient) {
   });
 
   Template.main.events({
-    'click #rate-button': updatePage
+    'click #rate-button': updatePage,
+    'click .word': function(evt) {
+      var word = evt.target.innerText;
+      Session.set('chosenWord', word);
+    }
+  });
+
+  Template.wordMenu.helpers({
+    'chosenWord': function() {
+      return Session.get('chosenWord') || 'N/A';
+    },
+    'chosenWordScore': function() {
+      var word = Words.findOne({word: Session.get('chosenWord')});
+      return (word && word.fluff) || 0;
+    }
+  });
+
+  Template.wordMenu.events({
+    'click #vote-up': function() {
+      var word = Session.get('chosenWord');
+      var w = Words.findOne({word: word});
+      if (!w) {
+        Words.insert({word: word, fluff: 1});
+      } else {
+        Words.update({_id: w._id}, {$set: {fluff: w.fluff + 1}});
+      }
+    },
+    'click #vote-down': function() {
+      var word = Session.get('chosenWord');
+      var w = Words.findOne({word: word});
+      if (!w) {
+        Words.insert({word: word, fluff: -1});
+      } else {
+        Words.update({_id: w._id}, {$set: {fluff: w.fluff - 1}});
+      }
+    }
   });
 }
 
@@ -73,7 +108,20 @@ fluffClass = function(word) {
 synthElements = function(words) {
   var elements = [];
   words.forEach(function(word) {
-    elements.push('<span class="' + fluffClass(word) + '">' + word + '</span>');
+    elements.push('<span class="word ' + fluffClass(word) + '">' + word + '</span>');
   });
   return elements.join(" ");
 };
+
+// wordMenu = function(word) {
+//   console.log("Chosen word:", word);
+//   var w = Words.findOne({word: word});
+//   w = w ? w : {word: word, fluff: 0};
+//   console.log(w);
+//   if (w._id) {
+//     Words.update({_id: w._id}, {$set: {fluff: w.fluff + 1}});
+//   } else {
+//     w.fluff += 1;
+//     Words.insert(w);
+//   }
+// };
